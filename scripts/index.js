6bd0f91aabcd1be92d1f713380bc5e7a07bea67c
Brandon Cullen <brandoncullen2201@gmail.com>
2023-08-20 14:05:08 -0400

    Added 'Changing color' section

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const changeColorBtn = document.querySelector('#changeColorBtn');
+const changeColor = document.querySelector('#changeColor');
+const showColor = document.querySelector('#showColor');
+
+function randomColor() {
+    let rgbValues = [];
+
+    for (let i = 0; i < 3; i++) {
+        rgbValues[i] = Math.floor(Math.random() * 255);
+    }
+
+    return `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
+}
+
+changeColorBtn.addEventListener('click', function() {
+    let color = randomColor();
+    changeColor.style.color = color;
+    showColor.innerHTML = "Color is: " + color;
+});
